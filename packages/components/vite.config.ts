ca3b989883b8b48875b9a7e7546317884c177fa8
benzic <690018019@qq.com>
2022-05-19 15:31:52 +0800

    init

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,23 +1,23 @@
 /*
  * @Author: Cookie
  * @Date: 2021-03-02 13:47:42
- * @LastEditors: Cookie
- * @LastEditTime: 2021-03-09 17:12:00
+ * @LastEditors: benzic 690018019@qq.com
+ * @LastEditTime: 2022-05-19 15:19:06
  * @FilePath: /vite-react-boty-desing/packages/components/vite.config.ts
  * @Description:
  */
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-const path = require('path');
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'boty-design',
-      formats:['es','umd','cjs']
-    }
-  }
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@adc-component",
+      formats: ["es", "umd", "cjs"],
+    },
+  },
 });
